60c2c24df1884ae7c879e7493a5b536f48ee5714
leegwae <leegwae@gmail.com>
2021-09-22 03:37:05 +0900

    Fix way to forward ref

--- src/components/plane/index.tsx
+++ src/components/plane/index.tsx
@@ -2,18 +2,19 @@ import * as THREE from 'three';
 import * as React from 'react';
 
 interface PlaneProps {
-	ref?: React.Ref<React.ReactNode>;
 	position?: THREE.Vector3;
 	rotation?: THREE.Euler;
 	args?: [width?: number, height?: number];
 }
 
-const Plane = ({ ref, position, rotation, args }: PlaneProps) => (
-	<mesh ref={ref} receiveShadow position={position} rotation={rotation}>
-		<boxBufferGeometry attach="geometry" args={args} />
-		<meshPhongMaterial attach="material" color="white" depthWrite />
-	</mesh>
-);
+const Plane = React.forwardRef<React.ReactNode, PlaneProps>(({ position, rotation, args }, ref) => {
+	return (
+		<mesh ref={ref} receiveShadow position={position} rotation={rotation}>
+			<boxBufferGeometry attach="geometry" args={args} />
+			<meshPhongMaterial attach="material" color="white" depthWrite />
+		</mesh>
+	);
+});
 
 export default Plane;
 
