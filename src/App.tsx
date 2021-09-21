22442953ddd9dc19a81078493e1315392a5fae00
leegwae <leegwae@gmail.com>
2021-09-22 01:32:12 +0900

    Refactor App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
 import * as THREE from 'three';
-import React, { useRef } from 'react';
-import { Canvas, useThree, useFrame, RootState } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import React, { useRef, ReactFragment } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { CameraControls, Cube, Plane } from './components';
 
-const CameraControls = () => {
-	const controls = useRef<any>();		// type 찾기
-	const { camera, gl: {domElement} } = useThree<RootState>();
-	useFrame((state) => controls.current.update());
+// 카메라 설정 관련 상수
+const SCREEN_WIDTH: number = window.innerWidth;
+const SCREEN_HEIGHT: number = window.innerHeight;
+const VIEW_ANGLE: number = 45;
+const ASPECT: number = SCREEN_WIDTH / SCREEN_HEIGHT;
+const NEAR: number = 1;
+const FAR: number = 10000;
 
-	return (
-		<OrbitControls
-			ref={controls}
-			args={[camera, domElement]}
-			enablePan={false}
-			enableZoom={false}
-			target={[0, 0.5, 0]}
+const ORIGIN: THREE.Vector3 = new THREE.Vector3(0, 0, 0);
+const CAMERA_POSITION: THREE.Vector3 = new THREE.Vector3(-3, 2, 5);
+
+interface SceneProps { children?: ReactFragment }
+const Scene = ({ children }: SceneProps) => (
+	<>
+		<CameraControls target={ORIGIN} />
+		<color attach="background" args={[new THREE.Color(0xa0a0a0)]} />
+		<fog attach="fog" color={new THREE.Color(0xa0a0a0)} near={10} far={50} />
+		<hemisphereLight
+			color={0xffffff}
+			groundColor={new THREE.Color(0x444444)}
+			position={[0, 20, 0]}
 		/>
-	);	
-};
+		<directionalLight
+			color={0xffffff}
+			position={[3, 10, 10]}
+			castShadow
+			shadow-camera-top={2}
+			shadow-camera-bottom={-2}
+			shadow-camera-left={-2}
+			shadow-camera-right={2}
+			shadow-camera-near={0.1}
+			shadow-camera-far={40}
+		/>
+		{children}
+	</>
+);
 
-function App() {
-	const boxMesh = useRef<THREE.Mesh>();
+const App = () => {
+	const cubeMesh = useRef<THREE.Mesh>();
 	const planeMesh = useRef<THREE.Mesh>();
 
 	return (
@@ -28,39 +49,29 @@ function App() {
 			<Canvas
 				gl={{ antialias: true }}
 				dpr={window.devicePixelRatio}
-				camera={{ position: [-3, 2, 5], fov: 90 }}
+				camera={
+					{
+						fov: VIEW_ANGLE,
+						aspect: ASPECT,
+						near: NEAR,
+						far: FAR,
+						position: CAMERA_POSITION,
+					}
+				}
 				shadows
 			>
-				<CameraControls />
-				<color attach="background" args={[new THREE.Color(0xa0a0a0)]} />
-				<fog attach="fog" color={new THREE.Color(0xa0a0a0)} near={10} far={50} />
-				<hemisphereLight
-					color={0xffffff}
-					groundColor={new THREE.Color(0x444444)}
-					position={[0, 20, 0]}
-				/>
-				<directionalLight
-					color={0xffffff}
-					position={[3, 10, 10]}
-					castShadow
-					shadow-camera-top={2}
-					shadow-camera-bottom={-2}
-					shadow-camera-left={-2}
-					shadow-camera-right={2}
-					shadow-camera-near={0.1}
-					shadow-camera-far={40}
-				/>
-				<mesh ref={boxMesh} castShadow>
-					<boxGeometry attach="geometry" args={[1, 1, 1]} />
-      				<meshStandardMaterial attach="material" color="orange" />
-				</mesh>
-				<mesh ref={planeMesh} receiveShadow position={[0, -1, 0]} rotation={[-Math.PI / 2, 0, 0]}>
-					<boxBufferGeometry attach="geometry" args={[1000, 1000]}/>
-					<meshPhongMaterial attach="material" color="white" depthWrite />	
-				</mesh>
+				<Scene>
+					<Cube ref={cubeMesh} position={ORIGIN} args={[1, 1, 1]} />
+					<Plane
+						ref={planeMesh}
+						position={new THREE.Vector3(0, -1, 0)}
+						rotation={new THREE.Euler(-Math.PI / 2, 0, 0)}
+						args={[1000, 1000]}
+					/>
+				</Scene>
 			</Canvas>
 		</>
 	);
-}
+};
 
 export default App;
